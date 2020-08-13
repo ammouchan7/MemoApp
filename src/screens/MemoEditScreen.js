8674c1da0b98007dc827501e90f1a2ffe3effa39
ammouchan7 <ysk.7727football@gmail.com>
2020-08-13 22:47:34 +0900

    Create MemoEditScreen

---
+++ src/screens/MemoEditScreen.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import { StyleSheet, View, Text, TextInput } from 'react-native';
+import CircleButton from '../elememts/CircleButton';
+
+class MemoEditScreen extends React.Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <TextInput style={styles.MemoEditInput} multiline value="hello" />
+        <CircleButton name="check" />
+      </View>
+    );
+  }
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    width: '100%',
+  },
+
+  MemoEditInput: {
+    backgroundColor: '#fff',
+    flex: 1,
+    paddingTop: 32,
+    paddingLeft: 16,
+    paddingRight: 16,
+    paddingBottom: 16,
+    fontSize: 16,
+
+  },
+});
+
+export default MemoEditScreen;
